713df71ca5933340fe37bb7c436c1b84fc76bab1
ali gelenler <aligelenler@alis-MBP.home>
2023-06-25 22:44:58 +0200

    Fixes to use JCE

--- config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -6,28 +6,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
+@Configuration
 public class SecurityConfig {
-
-	//@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	
-		return http.csrf().disable()
-				.authorizeHttpRequests()
-				.requestMatchers("/encrypt", "/decrypt","twitter-to-kafka-service/dev")
-				.permitAll()
-				.and()
-				.authorizeHttpRequests().anyRequest().authenticated().and().build();
-				
-				
-	}	
-		
-	
-
-
-	
-
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(requests -> requests
+						.requestMatchers(new AntPathRequestMatcher("/encrypt"),
+								new AntPathRequestMatcher("/decrypt")).permitAll()
+						.anyRequest().authenticated())
+				.httpBasic();
+		return http.build();
+	}
 }
